099af7592bd552ad813b384ec7b82a800e55ed56
Tarun Rochiramani <tarunrochiramani@gmail.com>
2018-02-10 14:02:51 -0800

    Minor change

--- src/test/java/com/tr/SetupSeedData.java
+++ src/test/java/com/tr/SetupSeedData.java
@@ -28,7 +28,7 @@ public class SetupSeedData extends AbstractSeedDataTest {
     @Test
     public void createRandomUsers() {
         User previousUser = null;
-        for(int i=1; i<=1000000; i++) {
+        for(int i=1; i<=10000; i++) {
             User randomUser = anUserBuilder().withUserName("user-" + i).withFirstName("user_first_" + i).withLastName("user_last_" + i).build();
             UUID randomUserId = createUser(randomUser);
 
